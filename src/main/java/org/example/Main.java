660ca54a2270bf637ada8c9890a996f58804c4ad
leofab <leofabr3@gmail.com>
2023-03-01 17:10:42 -0300

    valorFinalEmp method implement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,26 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
 
+    public static String valorFinalEmp(Integer valor, Integer parcelas){
+        if(parcelas >= 1 && parcelas <= 3){
+            double taxa = 0.9;
+            double valorFinal = valor / taxa;
+            double parcelasTotal = valorFinal / parcelas;
+            return "O valor final do emprestimo será de : " + String.format("%.2f", valorFinal) + " em " + parcelas + " parcelas de " + String.format("%.2f", parcelasTotal);
+        } else if(parcelas > 3 && parcelas <= 6){
+            double taxa = 0.7;
+            double valorFinal = valor / taxa;
+            double parcelasTotal = valorFinal / parcelas;
+            return "O valor final do emprestimo será de : " + String.format("%.2f", valorFinal) + " em " + parcelas + " parcelas de " + String.format("%.2f", parcelasTotal);
+        } else if (parcelas > 6 && parcelas <=12) {
+            double taxa = 0.3;
+            double valorFinal = valor / taxa;
+            double parcelasTotal = valorFinal / parcelas;
+            return "O valor final do emprestimo será de : " + String.format("%.2f", valorFinal) + " em " + parcelas + " parcelas de " + String.format("%.2f", parcelasTotal);
+        }
+        return "Insira um numero de parcelas entre 1 e 12 parcelas";
+    }
+
     public static String horaDoDia(){
         LocalTime horaAtual = LocalTime.now(ZoneId.of("America/Sao_Paulo"));
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HH:mm:ss");
@@ -31,5 +51,9 @@ public class Main {
 
         System.out.println(operacoes(4,5));
         System.out.println(horaDoDia());
+        System.out.println(valorFinalEmp(900,2));
+        System.out.println(valorFinalEmp(900,6));
+        System.out.println(valorFinalEmp(900,12));
+        System.out.println(valorFinalEmp(900,-1));
     }
 }
